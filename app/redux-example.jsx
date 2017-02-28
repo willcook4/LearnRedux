c7f5d9ba9433856ac3b8dea05ce558c199b2eb6b
Will Cook <hello@doubleucook.com>
2017-02-28 09:07:21 +0000

    converted actions to action generators

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,62 +1,9 @@
-var redux = require('redux');
+const redux = require('redux');
 
 console.log('Starting redux example');
 
-var stateDefault = {
-  name: 'Annonymous',
-  hobbies: [],
-  movies: []
-};
-var nextHobbyId = 1;
-var nextMovieId = 1;
-
-// var oldReducer = (state = stateDefault, action) => {
-//   // state = state || {name: 'Annonymous'}; Default state, This is now above in ES6 syntax
-//   // console.log('New Action', action);
-//   switch (action.type) {
-//   case 'CHANGE_NAME':
-//     return {
-//       ...state,
-//       name: action.name
-//     };
-//   case 'ADD_HOBBY':
-//     return {
-//       ...state,
-//       hobbies: [
-//         ...state.hobbies,
-//         {
-//           id: nextHobbyId++,
-//           hobbies: action.hobby
-//         }
-//       ]
-//     };
-//     case 'REMOVE_HOBBY':
-//       return {
-//         ...state,
-//         hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
-//       };
-//     case 'ADD_MOVIE':
-//       return {
-//         ...state,
-//         movies: [
-//           ...state.movies,
-//           {
-//             id: nextMovieId++,
-//             movieName: action.movie.name,
-//             movieGenre: action.movie.genre
-//           }
-//         ]
-//       };
-//     case 'REMOVE_MOVIE':
-//       return {
-//         ...state,
-//         movies: state.movies.filter((movie) => movie.id !== action.id)
-//       };
-//     default:
-//       return state; // No changes to state, always returns a state.
-//   }
-// };
-
+// Name reducer and action generator
+// ---------------------------------
 const nameReducer = (state = 'Annonymous', action) => {
   switch (action.type) {
     case 'CHANGE_NAME':
@@ -65,7 +12,17 @@ const nameReducer = (state = 'Annonymous', action) => {
       return state;
   }
 };
+// Action generator - Change name function
+var changeName = (name) => {
+  return {
+    type: 'CHANGE_NAME',
+    name // Same as 'name: name'
+  };
+};
 
+// Hobbies reducer and action generators
+// -------------------------------------
+let nextHobbyId = 1;
 const hobbiesReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_HOBBY':
@@ -83,6 +40,23 @@ const hobbiesReducer = (state = [], action) => {
   }
 };
 
+const addHobby = (hobby) => {
+  return {
+    type: 'ADD_HOBBY',
+    hobby
+  };
+};
+
+const removeHobby = (id) => {
+  return {
+    type: 'REMOVE_HOBBY',
+    id
+  };
+};
+
+// Movies reducer and action generators
+// -------------------------------------
+let nextMovieId = 1;
 const moviesReducer = (state= [], action) => {
   switch(action.type) {
     case 'ADD_MOVIE':
@@ -99,6 +73,20 @@ const moviesReducer = (state= [], action) => {
     default:
       return state;
   }
+};
+
+const addMovie = (movie) => {
+  return {
+    type: 'ADD_MOVIE',
+    movie
+  };
+};
+
+const removeMovie = (id) => {
+  return {
+    type: 'REMOVE_MOVIE',
+    id
+  };
 };
 
 const reducer = redux.combineReducers({
@@ -121,7 +109,7 @@ const store = redux.createStore(
 // (store) Object to store entire application, takes a reducer (a pure function).
 
 // Subscribe to changes
-var unsubscribe = store.subscribe(()=>{
+let unsubscribe = store.subscribe(()=>{
   var state = store.getState();
   console.log('Name is ', state.name);
   document.getElementById('app').innerHTML = state.name;
@@ -133,51 +121,58 @@ var unsubscribe = store.subscribe(()=>{
 var currentState = store.getState();
 console.log('currentState', currentState);
 
-store.dispatch({
-  type: 'CHANGE_NAME',
-  name: 'Andrew'
-});
+store.dispatch(changeName('Andrew'));
 
-store.dispatch({
-  type: 'ADD_HOBBY',
-  hobby: 'Running'
-});
+// store.dispatch({
+//   type: 'ADD_HOBBY',
+//   hobby: 'Running'
+// });
 
-store.dispatch({
-  type: 'ADD_HOBBY',
-  hobby: 'Walking'
-});
+// store.dispatch({
+//   type: 'ADD_HOBBY',
+//   hobby: 'Walking'
+// });
 
-store.dispatch({
-  type: 'REMOVE_HOBBY',
-  id: 2
-});
+store.dispatch(addHobby('Walking'));
+store.dispatch(addHobby('Running'));
 
-store.dispatch({
-  type: 'ADD_MOVIE',
-  movie: {
-    name: 'Titanic',
-    genre: 'Classic'
-  }
-});
+// store.dispatch({
+//   type: 'REMOVE_HOBBY',
+//   id: 2
+// });
+store.dispatch(removeHobby(2));
 
-store.dispatch({
-  type: 'CHANGE_NAME',
-  name: 'Emma'
-});
+// store.dispatch({
+//   type: 'ADD_MOVIE',
+//   movie: {
+//     name: 'Titanic',
+//     genre: 'Classic'
+//   }
+// });
+store.dispatch(addMovie({ name: 'Titanic', genre: 'Classic' }));
 
-store.dispatch({
-  type: 'ADD_MOVIE',
-  movie: {
-    name: 'Empire Strikes Back',
-    genre: 'Classic'
-  }
-});
+// store.dispatch({
+//   type: 'CHANGE_NAME',
+//   name: 'Emma'
+// });
 
-store.dispatch({
-  type: 'REMOVE_MOVIE',
-  id: 1
-});
+store.dispatch(changeName('Emma'));
+
+// store.dispatch({
+//   type: 'ADD_MOVIE',
+//   movie: {
+//     name: 'Empire Strikes Back',
+//     genre: 'Classic'
+//   }
+// });
+store.dispatch(addMovie({name: 'Empire Strikes Back', genre: 'Classic'}));
+
+// store.dispatch({
+//   type: 'REMOVE_MOVIE',
+//   id: 1
+// });
+
+store.dispatch(removeMovie(2));
 
 // console.log('Name should be Trevor', store.getState());
 
